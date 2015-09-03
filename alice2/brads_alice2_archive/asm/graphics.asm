05e05c79707a536591696aa149f0f062f6703f21
Brad Grantham <grantham@plunk.org>
2015-09-03 09:18:10 -0700

    break out test pattern function

--- alice2/brads_alice2_archive/asm/graphics.asm
+++ alice2/brads_alice2_archive/asm/graphics.asm
@@ -1,5 +1,5 @@
 #define SCREEN_ROWBYTES 22 
-#define SCREEN_FIRST_VISIBLE_ROW 20
+#define SCREEN_FIRST_VISIBLE_ROW 92
 #define SCREEN_FIRST_VISIBLE_COLUMN 3
 #define SCREEN_FIRST_VISIBLE_BYTE_OFFSET (SCREEN_ROWBYTES * SCREEN_FIRST_VISIBLE_ROW + SCREEN_FIRST_VISIBLE_COLUMN)
 
@@ -59,7 +59,7 @@ GRAYLP1
         RET
 
 ;
-;
+; Clear the framebuffer - heavy handed, clears all of graphics memory
 ;
 GFX_CLEAR
 
@@ -84,6 +84,57 @@ CLRLP2
 
 	DEC	A
 	JP	NZ, CLRLP1      ; outer loop continue
+
+	POP	HL
+	POP	DE
+	POP	BC
+	POP	AF
+
+	RET
+
+;
+; Store a bit-pattern test in graphics memory, assuming a multiple of 11 bytes
+;
+GFX_TESTPAT
+
+	PUSH	AF
+	PUSH	BC
+	PUSH	DE
+	PUSH	HL
+
+        LD      C, 0
+	LD	A, 32           
+	LD	HL, GFX_MEM + SCREEN_ROWBYTES * 120
+TESTPATLP1
+	LD	B, 23
+        
+TESTPATLP2
+	LD	(HL), 0FFh         ; store 0xFF byte
+	INC	HL              ; increment HL
+	LD	(HL), 0FFh         ; store 0xFF byte
+	INC	HL              ; increment HL
+	LD	(HL), 0         ; store 0 byte
+	INC	HL              ; increment HL
+	LD	(HL), H         ; store H of address of this byte
+	INC	HL              ; increment HL
+	LD	(HL), L         ; store L of address of this byte
+	INC	HL              ; increment HL
+	LD	(HL), 0         ; store 0 byte
+	INC	HL              ; increment HL
+	LD	(HL), 0         ; store 0 byte
+	INC	HL              ; increment HL
+	LD	(HL), A         ; store A of this loop pass
+	INC	HL              ; increment HL
+	LD	(HL), 0         ; store 0 byte
+	INC	HL              ; increment HL
+	LD	(HL), B         ; store B of this loop pass
+	INC	HL              ; increment HL
+	LD	(HL), 0         ; store 0 byte
+	INC	HL              ; increment HL
+	DJNZ	TESTPATLP2          ; inner loop continue
+
+	DEC	A
+	JP	NZ, TESTPATLP1      ; outer loop continue
 
 	POP	HL
 	POP	DE
