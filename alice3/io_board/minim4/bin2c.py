c4d108dc7406e5a4d9eb6b699707b39fc2a1e5bd
Brad Grantham <grantham@plunk.org>
2016-01-11 12:55:28 -0800

    store length correctly in bin2c.py

--- alice3/io_board/minim4/bin2c.py
+++ alice3/io_board/minim4/bin2c.py
@@ -5,9 +5,12 @@ file = open(sys.argv[2], "rb")
 
 print "unsigned char %s_bytes[] = {" % name
 
+length = 0
+
 bytes = file.read(16)
 while bytes:
+    length += len(bytes)
     print "    " + ", ".join([ ("0x%02x" % (ord(b))) for b in bytes]) + ", "
     bytes = file.read(16)
 print "};"
-print "unsigned int %s_length = %d;" % (name, len(bytes))
+print "unsigned int %s_length = %d;" % (name, length)
