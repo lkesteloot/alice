4edd3e5e16fedc2e3ec4f97eb038f300f9a1ce39
Brad Grantham <grantham@plunk.org>
2016-04-02 14:11:34 -0700

    break out command monitor input queue

---
+++ alice3/io_board/stm32f4/monitor_queue.c
@@ -0,0 +1,7 @@
+//----------------------------------------------------------------------------
+// Command monitor input queue
+
+#include "monitor_queue.h"
+
+volatile struct mon_queue_struct mon_queue;
+
