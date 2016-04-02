c820a8bba461a0d55a7e13c871daa82c823f3fb8
Brad Grantham <grantham@plunk.org>
2016-04-02 13:13:50 -0700

    move build ifdefs to defs.h

---
+++ alice3/io_board/stm32f4/defs.h
@@ -0,0 +1,20 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+// 1 for v1 and v2, 3 for v3
+#define ALICE3_V1 1
+#define ALICE3_V3 3
+#define ALICE3_VERSION  ALICE3_V3
+
+// if 1, then ARM reads and writes only A0-A15 and reads MREQ
+// if 0, then ARM uses A0-A17 and bus mastering to access external RAM,
+//   only writes MREQ
+#define ALICE3_V3_ARM_IS_RAM 0
+
+#define XSTR(x) STR(x)
+#define STR(x) # x
+
+#define enable_interrupts() __enable_irq()
+#define disable_interrupts() __disable_irq()
+
+#endif /* __MAIN_H__ */
