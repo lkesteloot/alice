5d5e0955c095ba030843168e4aaee3139df76d00
Brad Grantham <grantham@plunk.org>
2016-04-07 00:54:12 -0700

    inline and make sure to add memory barrier

--- alice3/io_board/stm32f4/gpio_helpers.h
+++ alice3/io_board/stm32f4/gpio_helpers.h
@@ -3,17 +3,19 @@
 
 #include <stm32f4xx_hal.h>
 
-inline void set_GPIO_value(GPIO_TypeDef* gpio, int mask, int value)
+static inline void set_GPIO_value(GPIO_TypeDef* gpio, int mask, int value)
 {
     unsigned long int data = value ? mask : 0;
     gpio->ODR = (gpio->ODR & ~mask) | data;
+    __asm__ volatile("" ::: "memory"); // Force all memory operations before to come before and all after to come after.
 }
 
-inline void set_GPIO_iotype(GPIO_TypeDef* gpio, int pin, unsigned int iotype)
+static inline void set_GPIO_iotype(GPIO_TypeDef* gpio, int pin, unsigned int iotype)
 {
     long unsigned int mask = ~(3U << (pin * 2));
     long unsigned int value = iotype << (pin * 2);
     gpio->MODER = (gpio->MODER & mask) | value;
+    __asm__ volatile("" ::: "memory"); // Force all memory operations before to come before and all after to come after.
 }
 
 #endif /* __GPIO_HELPERS__ */
