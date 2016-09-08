99d6db031f83510a11cf8903d09974f0f87623a3
Lawrence Kesteloot <lk@teamten.com>
2016-09-07 18:19:52 -0700

    Don't write off the edge of the VGA.

--- alice4/fpga/Alice4/Alice4.v
+++ alice4/fpga/Alice4/Alice4.v
@@ -307,7 +307,7 @@ wire Vga_fifo_rdempty;
 wire Vga_fifo_wrfull;
 wire [7:0] Vga_fifo_wrusedw;
 
-assign Vga_fifo_input = (X == 0 || X == 639 || Y == 0 || Y == 479) ? 12'hFFF :
+assign Vga_fifo_input = (X == 0 || X == 639 || Y == 0 || Y == 479) ? 12'hF00 :
 	(X[4] ^ Y[4]) ? 12'h000 : 12'hFFF;
 	//assign Vga_fifo_input = (X == 0 || X == 639) ? 12'hFFF : Y == 0 ? 12'hFF0 : Y == 479 ? 12'hF0F :
 	//(X[4] ^ Y[4]) ? 12'hF00 : 12'h0FF;
