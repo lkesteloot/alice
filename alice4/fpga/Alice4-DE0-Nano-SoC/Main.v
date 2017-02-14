3517c4834b60948f3596fab6fc3e11ee4e07a990
Lawrence Kesteloot <lk@teamten.com>
2017-02-13 20:41:41 -0800

    Switch to lower case parameter names.

--- alice4/fpga/Alice4-DE0-Nano-SoC/Main.v
+++ alice4/fpga/Alice4-DE0-Nano-SoC/Main.v
@@ -6,118 +6,116 @@
 //`define enable_HPS
 
 module Main(
-
-	//////////// CLOCK //////////
-	input 		          		CLOCK_50,
-	input 		          		CLOCK2_50,
-	input 		          		CLOCK3_50,
+        //////////// CLOCK //////////
+        input clock_50,
+        input clock2_50,
+        input clock3_50,
 
 `ifdef enable_ADC
-	//////////// ADC //////////
-	/* 3.3-V LVTTL */
-	output		          		ADC_CONVST,
-	output		          		ADC_SCLK,
-	output		          		ADC_SDI,
-	input 		          		ADC_SDO,
+        //////////// ADC //////////
+        /* 3.3-V LVTTL */
+        output adc_convst,
+        output adc_sclk,
+        output adc_sdi,
+        input adc_sdo,
 `endif
-	
+        
 `ifdef enable_ARDUINO
-	//////////// ARDUINO ////////////
-	/* 3.3-V LVTTL */
-	inout					[15:0]	ARDUINO_IO,
-	inout								ARDUINO_RESET_N,
+        //////////// ARDUINO ////////////
+        /* 3.3-V LVTTL */
+        inout [15:0] arduino_io,
+        inout arduino_reset_n,
 `endif
-	
+        
 `ifdef enable_GPIO0
-	//////////// GPIO 0 ////////////
-	/* 3.3-V LVTTL */
-	inout				[35:0]		GPIO_0,
+        //////////// GPIO 0 ////////////
+        /* 3.3-V LVTTL */
+        inout [35:0] gpio_0,
 `endif
 
-`ifdef enable_GPIO1	
-	//////////// GPIO 1 ////////////
-	/* 3.3-V LVTTL */
-	inout				[35:0]		GPIO_1,
+`ifdef enable_GPIO1     
+        //////////// GPIO 1 ////////////
+        /* 3.3-V LVTTL */
+        inout [35:0] gpio_1,
 `endif
 
 `ifdef enable_HPS
-	//////////// HPS //////////
-	/* 3.3-V LVTTL */
-	inout 		          		HPS_CONV_USB_N,
-	
-	/* SSTL-15 Class I */
-	output		    [14:0]		HPS_DDR3_ADDR,
-	output		     [2:0]		HPS_DDR3_BA,
-	output		          		HPS_DDR3_CAS_N,
-	output		          		HPS_DDR3_CKE,
-	output		          		HPS_DDR3_CS_N,
-	output		     [3:0]		HPS_DDR3_DM,
-	inout 		    [31:0]		HPS_DDR3_DQ,
-	output		          		HPS_DDR3_ODT,
-	output		          		HPS_DDR3_RAS_N,
-	output		          		HPS_DDR3_RESET_N,
-	input 		          		HPS_DDR3_RZQ,
-	output		          		HPS_DDR3_WE_N,
-	/* DIFFERENTIAL 1.5-V SSTL CLASS I */
-	output		          		HPS_DDR3_CK_N,
-	output		          		HPS_DDR3_CK_P,
-	inout 		     [3:0]		HPS_DDR3_DQS_N,
-	inout 		     [3:0]		HPS_DDR3_DQS_P,
-	
-	/* 3.3-V LVTTL */
-	output		          		HPS_ENET_GTX_CLK,
-	inout 		          		HPS_ENET_INT_N,
-	output		          		HPS_ENET_MDC,
-	inout 		          		HPS_ENET_MDIO,
-	input 		          		HPS_ENET_RX_CLK,
-	input 		     [3:0]		HPS_ENET_RX_DATA,
-	input 		          		HPS_ENET_RX_DV,
-	output		     [3:0]		HPS_ENET_TX_DATA,
-	output		          		HPS_ENET_TX_EN,
-	inout 		          		HPS_GSENSOR_INT,
-	inout 		          		HPS_I2C0_SCLK,
-	inout 		          		HPS_I2C0_SDAT,
-	inout 		          		HPS_I2C1_SCLK,
-	inout 		          		HPS_I2C1_SDAT,
-	inout 		          		HPS_KEY,
-	inout 		          		HPS_LED,
-	inout 		          		HPS_LTC_GPIO,
-	output		          		HPS_SD_CLK,
-	inout 		          		HPS_SD_CMD,
-	inout 		     [3:0]		HPS_SD_DATA,
-	output		          		HPS_SPIM_CLK,
-	input 		          		HPS_SPIM_MISO,
-	output		          		HPS_SPIM_MOSI,
-	inout 		          		HPS_SPIM_SS,
-	input 		          		HPS_UART_RX,
-	output		          		HPS_UART_TX,
-	input 		          		HPS_USB_CLKOUT,
-	inout 		     [7:0]		HPS_USB_DATA,
-	input 		          		HPS_USB_DIR,
-	input 		          		HPS_USB_NXT,
-	output		          		HPS_USB_STP,
+        //////////// HPS //////////
+        /* 3.3-V LVTTL */
+        inout hps_conv_usb_n,
+        
+        /* SSTL-15 Class I */
+        output [14:0] hps_ddr3_addr,
+        output [2:0] hps_ddr3_ba,
+        output hps_ddr3_cas_n,
+        output hps_ddr3_cke,
+        output hps_ddr3_cs_n,
+        output [3:0] hps_ddr3_dm,
+        inout [31:0] hps_ddr3_dq,
+        output hps_ddr3_odt,
+        output hps_ddr3_ras_n,
+        output hps_ddr3_reset_n,
+        input hps_ddr3_rzq,
+        output hps_ddr3_we_n,
+        /* DIFFERENTIAL 1.5-V SSTL CLASS I */
+        output hps_ddr3_ck_n,
+        output hps_ddr3_ck_p,
+        inout [3:0] hps_ddr3_dqs_n,
+        inout [3:0] hps_ddr3_dqs_p,
+        
+        /* 3.3-V LVTTL */
+        output hps_enet_gtx_clk,
+        inout hps_enet_int_n,
+        output hps_enet_mdc,
+        inout hps_enet_mdio,
+        input hps_enet_rx_clk,
+        input [3:0] hps_enet_rx_data,
+        input hps_enet_rx_dv,
+        output [3:0] hps_enet_tx_data,
+        output hps_enet_tx_en,
+        inout hps_gsensor_int,
+        inout hps_i2c0_sclk,
+        inout hps_i2c0_sdat,
+        inout hps_i2c1_sclk,
+        inout hps_i2c1_sdat,
+        inout hps_key,
+        inout hps_led,
+        inout hps_ltc_gpio,
+        output hps_sd_clk,
+        inout hps_sd_cmd,
+        inout [3:0] hps_sd_data,
+        output hps_spim_clk,
+        input hps_spim_miso,
+        output hps_spim_mosi,
+        inout hps_spim_ss,
+        input hps_uart_rx,
+        output hps_uart_tx,
+        input hps_usb_clkout,
+        inout [7:0] hps_usb_data,
+        input hps_usb_dir,
+        input hps_usb_nxt,
+        output hps_usb_stp,
 `endif
-	
-	//////////// KEY ////////////
-	/* 3.3-V LVTTL */
-	input				[1:0]			KEY,
-	
-	//////////// LED ////////////
-	/* 3.3-V LVTTL */
-	output			[7:0]			LED,
-	
-	//////////// SW ////////////
-	/* 3.3-V LVTTL */
-	input				[3:0]			SW
-
+        
+        //////////// KEY ////////////
+        /* 3.3-V LVTTL */
+        input [1:0] key,
+        
+        //////////// LED ////////////
+        /* 3.3-V LVTTL */
+        output [7:0] led,
+        
+        //////////// SW ////////////
+        /* 3.3-V LVTTL */
+        input [3:0] sw
 );
 
 reg [23:0] counter;
 
-always @(posedge CLOCK_50) begin
+always @(posedge clock_50) begin
    counter <= counter + 1;
 end
 
-assign LED[0] = counter[23];
+assign led[0] = counter[23];
 
 endmodule
