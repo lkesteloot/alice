ade970bc9d87afe042426a837a8f7e1e7b2108f2
Lawrence Kesteloot <lk@teamten.com>
2017-02-27 23:06:18 -0800

    Add basic character font for LCD.

--- alice4/fpga/Alice4-DE0-Nano-SoC/LCD_test.v
+++ alice4/fpga/Alice4-DE0-Nano-SoC/LCD_test.v
@@ -10,10 +10,35 @@ module LCD_test(
     output [7:0] blue
 );
 
-    wire checkerboard = x[4] ^ y[4];
+wire checkerboard = x[4] ^ y[4];
+reg bw;
+wire [7:0] gray = bw ? 8'h80 : 8'h00;
+assign red = gray;
+assign green = gray;
+assign blue = gray;
 
-    assign red = checkerboard ? 8'h80 : 8'h00;
-    assign green = checkerboard ? 8'h80 : 8'h00;
-    assign blue = checkerboard ? 8'h80 : 8'h00;
+// Break screen down into characters.
+wire [5:0] row = y[9:4];
+wire [6:0] column = x[9:3];
+wire [3:0] character_row = y[3:0];
+wire [2:0] character_column = x[2:0];
+
+// Figure out what character to draw.
+wire [12:0] address = { row, column };
+wire [6:0] character = address[6:0] ^ (row[0] ? 7'h40 : 7'h00);
+
+// Look up character and bit.
+wire [10:0] font_addr = { character, character_row };
+wire [7:0] font_data;
+Font_ROM fontROM(clock, font_addr, font_data);
+
+always @(*) begin
+    if (0) begin
+        bw <= checkerboard;
+    end else begin
+        // The ~ trick is to reverse the order of the bits in font_data.
+        bw <= font_data[~character_column];
+    end
+end
 
 endmodule
