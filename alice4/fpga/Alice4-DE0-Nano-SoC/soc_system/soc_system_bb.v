be5fdc54639e59a2ed9d22213c913f544304989d
Lawrence Kesteloot <lk@teamten.com>
2017-03-04 01:32:23 -0800

    Simplify soc_system interface by removing unused AXI interfaces.

--- alice4/fpga/Alice4-DE0-Nano-SoC/soc_system/soc_system_bb.v
+++ alice4/fpga/Alice4-DE0-Nano-SoC/soc_system/soc_system_bb.v
@@ -1,6 +1,15 @@
 
 module soc_system (
 	clk_clk,
+	hps_0_f2h_sdram0_data_address,
+	hps_0_f2h_sdram0_data_burstcount,
+	hps_0_f2h_sdram0_data_waitrequest,
+	hps_0_f2h_sdram0_data_readdata,
+	hps_0_f2h_sdram0_data_readdatavalid,
+	hps_0_f2h_sdram0_data_read,
+	hps_0_f2h_sdram0_data_writedata,
+	hps_0_f2h_sdram0_data_byteenable,
+	hps_0_f2h_sdram0_data_write,
 	memory_mem_a,
 	memory_mem_ba,
 	memory_mem_ck,
@@ -16,18 +25,18 @@ module soc_system (
 	memory_mem_dqs_n,
 	memory_mem_odt,
 	memory_mem_dm,
-	memory_oct_rzqin,
-	hps_0_f2h_sdram0_data_address,
-	hps_0_f2h_sdram0_data_burstcount,
-	hps_0_f2h_sdram0_data_waitrequest,
-	hps_0_f2h_sdram0_data_readdata,
-	hps_0_f2h_sdram0_data_readdatavalid,
-	hps_0_f2h_sdram0_data_read,
-	hps_0_f2h_sdram0_data_writedata,
-	hps_0_f2h_sdram0_data_byteenable,
-	hps_0_f2h_sdram0_data_write);	
+	memory_oct_rzqin);	
 
 	input		clk_clk;
+	input	[28:0]	hps_0_f2h_sdram0_data_address;
+	input	[7:0]	hps_0_f2h_sdram0_data_burstcount;
+	output		hps_0_f2h_sdram0_data_waitrequest;
+	output	[63:0]	hps_0_f2h_sdram0_data_readdata;
+	output		hps_0_f2h_sdram0_data_readdatavalid;
+	input		hps_0_f2h_sdram0_data_read;
+	input	[63:0]	hps_0_f2h_sdram0_data_writedata;
+	input	[7:0]	hps_0_f2h_sdram0_data_byteenable;
+	input		hps_0_f2h_sdram0_data_write;
 	output	[14:0]	memory_mem_a;
 	output	[2:0]	memory_mem_ba;
 	output		memory_mem_ck;
@@ -44,13 +53,4 @@ module soc_system (
 	output		memory_mem_odt;
 	output	[3:0]	memory_mem_dm;
 	input		memory_oct_rzqin;
-	input	[28:0]	hps_0_f2h_sdram0_data_address;
-	input	[7:0]	hps_0_f2h_sdram0_data_burstcount;
-	output		hps_0_f2h_sdram0_data_waitrequest;
-	output	[63:0]	hps_0_f2h_sdram0_data_readdata;
-	output		hps_0_f2h_sdram0_data_readdatavalid;
-	input		hps_0_f2h_sdram0_data_read;
-	input	[63:0]	hps_0_f2h_sdram0_data_writedata;
-	input	[7:0]	hps_0_f2h_sdram0_data_byteenable;
-	input		hps_0_f2h_sdram0_data_write;
 endmodule
--- alice4/fpga/Alice4-DE0-Nano-SoC/soc_system/soc_system_inst.v
+++ alice4/fpga/Alice4-DE0-Nano-SoC/soc_system/soc_system_inst.v
@@ -1,5 +1,14 @@
 	soc_system u0 (
 		.clk_clk                             (<connected-to-clk_clk>),                             //                   clk.clk
+		.hps_0_f2h_sdram0_data_address       (<connected-to-hps_0_f2h_sdram0_data_address>),       // hps_0_f2h_sdram0_data.address
+		.hps_0_f2h_sdram0_data_burstcount    (<connected-to-hps_0_f2h_sdram0_data_burstcount>),    //                      .burstcount
+		.hps_0_f2h_sdram0_data_waitrequest   (<connected-to-hps_0_f2h_sdram0_data_waitrequest>),   //                      .waitrequest
+		.hps_0_f2h_sdram0_data_readdata      (<connected-to-hps_0_f2h_sdram0_data_readdata>),      //                      .readdata
+		.hps_0_f2h_sdram0_data_readdatavalid (<connected-to-hps_0_f2h_sdram0_data_readdatavalid>), //                      .readdatavalid
+		.hps_0_f2h_sdram0_data_read          (<connected-to-hps_0_f2h_sdram0_data_read>),          //                      .read
+		.hps_0_f2h_sdram0_data_writedata     (<connected-to-hps_0_f2h_sdram0_data_writedata>),     //                      .writedata
+		.hps_0_f2h_sdram0_data_byteenable    (<connected-to-hps_0_f2h_sdram0_data_byteenable>),    //                      .byteenable
+		.hps_0_f2h_sdram0_data_write         (<connected-to-hps_0_f2h_sdram0_data_write>),         //                      .write
 		.memory_mem_a                        (<connected-to-memory_mem_a>),                        //                memory.mem_a
 		.memory_mem_ba                       (<connected-to-memory_mem_ba>),                       //                      .mem_ba
 		.memory_mem_ck                       (<connected-to-memory_mem_ck>),                       //                      .mem_ck
@@ -15,15 +24,6 @@
 		.memory_mem_dqs_n                    (<connected-to-memory_mem_dqs_n>),                    //                      .mem_dqs_n
 		.memory_mem_odt                      (<connected-to-memory_mem_odt>),                      //                      .mem_odt
 		.memory_mem_dm                       (<connected-to-memory_mem_dm>),                       //                      .mem_dm
-		.memory_oct_rzqin                    (<connected-to-memory_oct_rzqin>),                    //                      .oct_rzqin
-		.hps_0_f2h_sdram0_data_address       (<connected-to-hps_0_f2h_sdram0_data_address>),       // hps_0_f2h_sdram0_data.address
-		.hps_0_f2h_sdram0_data_burstcount    (<connected-to-hps_0_f2h_sdram0_data_burstcount>),    //                      .burstcount
-		.hps_0_f2h_sdram0_data_waitrequest   (<connected-to-hps_0_f2h_sdram0_data_waitrequest>),   //                      .waitrequest
-		.hps_0_f2h_sdram0_data_readdata      (<connected-to-hps_0_f2h_sdram0_data_readdata>),      //                      .readdata
-		.hps_0_f2h_sdram0_data_readdatavalid (<connected-to-hps_0_f2h_sdram0_data_readdatavalid>), //                      .readdatavalid
-		.hps_0_f2h_sdram0_data_read          (<connected-to-hps_0_f2h_sdram0_data_read>),          //                      .read
-		.hps_0_f2h_sdram0_data_writedata     (<connected-to-hps_0_f2h_sdram0_data_writedata>),     //                      .writedata
-		.hps_0_f2h_sdram0_data_byteenable    (<connected-to-hps_0_f2h_sdram0_data_byteenable>),    //                      .byteenable
-		.hps_0_f2h_sdram0_data_write         (<connected-to-hps_0_f2h_sdram0_data_write>)          //                      .write
+		.memory_oct_rzqin                    (<connected-to-memory_oct_rzqin>)                     //                      .oct_rzqin
 	);
 
