d7c6a09875f2705ed49f6be4d3a7fb6193ed2fac
Brad Grantham <grantham@plunk.org>
2017-04-01 02:25:25 -0700

    move network functions to the right module

--- alice4/software/libgl/connection.h
+++ alice4/software/libgl/connection.h
@@ -2,11 +2,18 @@
 #ifndef __CONNECTION_H__
 #define __CONNECTION_H__
 
-// Interface for connection (to network or FPGA).
+// Interface for connection to network
 void open_connection();
 void send_uint8(unsigned char b);
 unsigned char receive_uint8();
 void flush();
 void close_connection();
+void send_and_capture_string(char *s);
+void send_and_capture_uint16(uint16_t x);
+void send_and_capture_uint32(uint32_t x);
+void send_uint16(uint16_t x);
+void send_uint32(uint32_t x);
+uint16_t receive_uint16();
+uint32_t receive_uint32();
 
 #endif // __CONNECTION_H__
