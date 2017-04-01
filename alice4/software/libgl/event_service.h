e6083cb7179c23930dc8aad4d05e05d410ed2dcd
Brad Grantham <grantham@plunk.org>
2017-04-01 02:17:29 -0700

    Remove vi schmutz at EOF

--- alice4/software/libgl/event_service.h
+++ alice4/software/libgl/event_service.h
@@ -11,4 +11,4 @@ int32_t events_qread_start(int blocking);
 int32_t events_qread_continue(int16_t *value);
 void events_tie(int32_t button, int32_t val1, int32_t val2);
 
-#endif /* __EVENT_SERVICE_H__ */:w
+#endif /* __EVENT_SERVICE_H__ */
