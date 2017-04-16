acf916b48b9f1dbe28392aa816a87cdd85157a63
Brad Grantham <grantham@plunk.org>
2017-04-16 22:25:58 +0000

    Add ERROR prints on unexpected conditions

--- alice4/software/touchscreen_tests/ts_test1.cpp
+++ alice4/software/touchscreen_tests/ts_test1.cpp
@@ -9,6 +9,7 @@ int main()
     touchscreen_init();
 
     while(1) {
+	static bool dragging = false;
 	int x, y;
 	float z;
 
@@ -17,12 +18,20 @@ int main()
 	    switch(e) {
 		case TOUCHSCREEN_START:
 		    printf("start %d %d %f\n", x, y, z);
+		    if(dragging)
+			printf("ERROR: START WHILE ALREADY DRAGGING\n");
+		    dragging = true;
 		    break;
 		case TOUCHSCREEN_DRAG:
 		    printf("drag %d %d %f\n", x, y, z);
+		    if(!dragging)
+			printf("ERROR: DRAG WHILE NOT STARTED\n");
 		    break;
 		case TOUCHSCREEN_STOP:
 		    printf("stop\n");
+		    if(!dragging)
+			printf("ERROR: STOP WHILE NOT STARTED\n");
+		    dragging = false;
 		    break;
 		case TOUCHSCREEN_IDLE:
 		default:
