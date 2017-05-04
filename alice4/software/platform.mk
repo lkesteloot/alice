1f43fc8de573fa9e9b925da0e6976bd9e034aec1
Lawrence Kesteloot <lk@teamten.com>
2017-05-04 11:16:25 -0700

    Auto-detect platform in makefile.

--- alice4/software/platform.mk
+++ alice4/software/platform.mk
@@ -1,10 +1,15 @@
-# NOTE put libgl/network.c in only once
 
-RASTERIZER_SRCS = ../libgl/network_rasterizer.c ../libgl/network.c
-EVENTS_SRCS = ../libgl/network_events.c # ../libgl/network.c
+UNAME := $(shell uname)
+
+ifeq ($(UNAME),Darwin)
+	# NOTE put libgl/network.c in only once
+	RASTERIZER_SRCS = ../libgl/network_rasterizer.c ../libgl/network.c
+	EVENTS_SRCS = ../libgl/network_events.c # ../libgl/network.c
+else
+	RASTERIZER_SRCS = ../libgl/hardware_rasterizer.c
+	EVENTS_SRCS = ../libgl/hardware_events.c ../libgl/touchscreen.c
+endif
 
 # RASTERIZER_SRCS = ../libgl/reference_rasterizer.c
 # EVENTS_SRCS = ../libgl/hardware_events.c ../libgl/touchscreen.c
 
-RASTERIZER_SRCS = ../libgl/hardware_rasterizer.c
-EVENTS_SRCS = ../libgl/hardware_events.c ../libgl/touchscreen.c
