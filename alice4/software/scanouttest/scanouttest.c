38dcf5682f75996fa02aaab763b3d83d361fbaf1
Brad Grantham <grantham@plunk.org>
2017-05-17 14:52:01 -0700

    Add modes for just ramp and for delaying all bits
    Change size to 800x480 so that output matches Alice 4 display
        - notably the ramp can be loaded into Alice 4 display to illustrate issues
    
    "make" creates all three images.
    
    Provide "--all8bits" to delay all 8 bits of the color, showing close to what Brad sees.
    
    Provide "--justramp" to output the color ramp with no output effect.

--- alice4/software/scanouttest/scanouttest.c
+++ alice4/software/scanouttest/scanouttest.c
@@ -1,26 +1,53 @@
-
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("P6 256 256 255\n");
-    for (int y = 0; y < 128; y++) {
-        int color = 0;
-        int previousColor = 0;
-        for (int x = 0; x < 256; x++) {
-            int outputColor = color | (previousColor & 0x40);
-            printf("%c%c%c", outputColor, outputColor, outputColor);
-            previousColor = color;
-            color++;
+typedef enum {NONE, JUST_BIT_6, ALL_8_BITS} Effect;
+
+int do_effect(int previousColor, int color, Effect effect)
+{
+    int outputColor = color;
+    switch(effect) {
+        case NONE:
+            break;
+        case JUST_BIT_6:
+            outputColor = outputColor | (previousColor & 0x40);
+            break;
+        case ALL_8_BITS:
+            for(int b = 0; b < 8; b++)
+                outputColor = outputColor | (previousColor & (1 << b));
+            break;
+    }
+    return outputColor;
+}
+
+int main(int argc, char **argv) {
+    Effect effect = JUST_BIT_6;
+    while(argc > 1) {
+        if(strcmp(argv[1], "--all8bits") == 0) {
+            effect = ALL_8_BITS;
+            argc--; argv++;
+        } else if(strcmp(argv[1], "--justramp") == 0) {
+            effect = NONE;
+            argc--; argv++;
         }
     }
-    for (int y = 0; y < 128; y++) {
-        int color = 255;
-        int previousColor = 255;
-        for (int x = 0; x < 256; x++) {
-            int outputColor = color | (previousColor & 0x40);
+    printf("P6 800 480 255\n");
+    for (int y = 0; y < 240; y++) {
+        int previousColor = 0;
+        for (int x = 0; x < 800; x++) {
+            int color = x * 256 / 800;
+            int outputColor = do_effect(previousColor, color, effect);
             printf("%c%c%c", outputColor, outputColor, outputColor);
             previousColor = color;
-            color--;
+        }
+    }
+    for (int y = 0; y < 240; y++) {
+        int previousColor = 255;
+        for (int x = 0; x < 800; x++) {
+            int color = 255 - x * 256 / 800;
+            int outputColor = do_effect(previousColor, color, effect);
+            printf("%c%c%c", outputColor, outputColor, outputColor);
+            previousColor = color;
         }
     }
 
