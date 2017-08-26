bb5ac1cbbaeedcb99ef7c1bd9ec759861889e73b
Brad Grantham <grantham@plunk.org>
2017-08-25 18:47:04 -0700

    set up RFSH and WAIT signals correctly

--- alice3/io_board/stm32f4/bus_signals.c
+++ alice3/io_board/stm32f4/bus_signals.c
@@ -96,13 +96,19 @@ void BUS_init()
     HAL_GPIO_Init(BUS_RESET_PORT, &GPIO_InitStruct); 
     HAL_GPIO_WritePin(BUS_RESET_PORT, BUS_RESET_PIN_MASK, BUS_RESET_ACTIVE);
 
-    // DEBUG on C15
-    GPIO_InitStruct.Pin = GPIO_PIN_15;
+    // /WAIT output on D2
+    GPIO_InitStruct.Pin = BUS_WAIT_PIN_MASK;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct); 
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, 0);
+    HAL_GPIO_Init(BUS_WAIT_PORT, &GPIO_InitStruct); 
+    HAL_GPIO_WritePin(BUS_WAIT_PORT, BUS_WAIT_PIN_MASK, BUS_WAIT_INACTIVE);
+
+    // /RFSH input on C15
+    GPIO_InitStruct.Pin = BUS_RFSH_PIN_MASK;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(BUS_RFSH_PORT, &GPIO_InitStruct); 
 
     // Address bus pins
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
